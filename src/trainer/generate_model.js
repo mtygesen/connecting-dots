446e396049f10087902923120980efd5fc84f17c
icecoldgold773 <mikkel.tygesen@gmail.com>
2023-04-08 01:42:32 +0200

    Fixed wording

--- src/trainer/generate_model.js
+++ src/trainer/generate_model.js
@@ -1,5 +1,5 @@
 /**
- * Script that setups, trains and tests a convolutional neural network on the mnist dataset, and saves the model as a json file.
+ * Script that sets up, trains and tests a convolutional neural network on the mnist dataset, and saves the model as a json file.
  * The configuration can be changed in model_settings.json
  */
 
