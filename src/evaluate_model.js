e4cb571271e56e3ff7ab406169f273f716fc4c15
Mikkel Tygesen <83021047+icecoldgold773@users.noreply.github.com>
2023-04-11 19:52:24 +0200

    Update evaluate_model.js

--- src/evaluate_model.js
+++ src/evaluate_model.js
@@ -1,5 +1,5 @@
 import convnetjs from 'convnetjs';
-import { GetModel } from './load_model.js';
+import { LoadModel } from './load_model.js';
 
 /**
  * Async function that evaluates the result of a model given an input image
@@ -10,7 +10,7 @@ import { GetModel } from './load_model.js';
  * @returns a promise with an array of probabilities
  */
 async function EvaluateModel(modelName, input) {
-    const model = (await GetModel(modelName)).default;
+    const model = (await LoadModel(modelName)).default;
 
     const digit = new convnetjs.Vol(28, 28, 1, 0.0);
 
