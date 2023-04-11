44c43a52f6fde2091e84348293e9e3b14d033e8b
icecoldgold773 <mikkel.tygesen@gmail.com>
2023-04-11 18:48:43 +0200

    Added display prediction

--- src/load_model.js
+++ src/load_model.js
@@ -1,3 +1,10 @@
+/**
+ * Loads a model from the models folder
+ * 
+ * @param modelName name of the model to load
+ * 
+ * @returns a promise with the model object 
+ */
 async function GetModel(modelName) {
     return await import(`./trainer/models/${modelName}.json`, { assert: { type: 'json' }});
 }
