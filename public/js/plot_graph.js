f949c12377e4abf1457dd78774e0bd778ab4aabc
Kneckerino <mrenne22@student.aau.dk>
2023-04-13 00:04:13 +0200

    PlotGraph() is assumed finished and scalable

--- public/js/plot_graph.js
+++ public/js/plot_graph.js
@@ -1,15 +1,22 @@
 /**
- * Makes a plot of the loss over iterations from the given training algorithm
+ * Makes a plot of the loss over iterations from the given training algorithm in the canvas with id "LossChart"
  * 
  * @param model object
  * 
  * @returns void
  */
 function PlotGraph() {
-    let canvas = document.getElementById("LossChart")
-    let ctx  = canvas.getContext("2d")
+    // Find and initialize the canvas
+    const canvas = document.getElementById("LossChart")
+    const ctx  = canvas.getContext("2d")
 
-    let model = {"stats": [
+    // Upscale the properties by factor
+    const scale = 4
+    canvas.width = canvas.clientWidth * scale
+    canvas.height = canvas.clientHeight * scale
+    ctx.lineWidth = 2 * scale
+
+    const model = {"stats": [
         {
             "fwd_time": 4,
             "bwd_time": 6,
@@ -902,19 +909,84 @@ function PlotGraph() {
             "loss": 0.10805403979994561
         }]}
 
-    let height = canvas.clientHeight
-    let points = model.stats.length
-    let pxlDlta = canvas.clientWidth / (points - 1) //there are points - 1 lines
-
-
+    // Find the max value of the loss from the samples which will be the peak of the Y-coordinates
     const max = Math.max(...model.stats.map((stat) => stat.loss));
-    console.log("max is " + max)
+    
 
+    // initialize values for the grid
+    const coloumns = 10
+    const maxColoumns = 12
+    const xDivide = canvas.width / maxColoumns
+    const rows = 10
+    const maxRows = 15
+    const yDivide = canvas.height / maxRows
+
+    const grid = {};
+    grid.length = xDivide * coloumns
+    grid.height = yDivide * rows
+    grid.left = xDivide * (maxColoumns - coloumns) / 2
+    grid.top = yDivide * (maxRows - rows) / 2
+    grid.right = canvas.width - grid.left
+    grid.bottom = canvas.height - grid.top
+
+
+    
+    // Grid background
+    ctx.beginPath()
+    ctx.fillStyle = "#FFF";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // The grid will be centered on the canvas
+    ctx.beginPath()
+    ctx.strokeStyle = "#000"
+    for (let i = 0; i <= coloumns; i++) { // Coloumns
+        let x = grid.left + xDivide * i
+        ctx.moveTo(x, grid.top)
+        ctx.lineTo(x, grid.bottom)
+        ctx.stroke()
+    } 
+    for (let i = 0; i <= rows; i++) { // Rows
+        let y = grid.top + yDivide * i
+        ctx.moveTo(grid.left, y)
+        ctx.lineTo(grid.right, y)
+        ctx.stroke()
+    }
+    
+
+    // Write labels
+    ctx.beginPath()
+    ctx.fillStyle = "#000";
+    const fontsize = Math.min(0.5 * yDivide, xDivide / 3) // Find correct fontsize
+    ctx.font = `${fontsize}px Arial`
+    ctx.textAlign = "right"
+    for (let i = 0; i <= rows; i++) { // Y
+        let x = grid.left * 0.9
+        let y = grid.top + yDivide * i
+        let value = (1 - i / rows) * max
+        ctx.fillText(value.toFixed(2), x, y);
+    }
+    ctx.textAlign = "center"
+    for (let i = 0; i <= coloumns; i++) { // X
+        let x = grid.left + xDivide * i
+        let y = grid.bottom + grid.top * 0.25
+        ctx.fillText(10 * i, x, y);
+    }
+
+    // Draw the actual graph
+    const points = model.stats.length
+    const pxlDlta = grid.length / (points - 1) // There are points - 1 lines
+
+    ctx.beginPath()
+    ctx.strokeStyle = "#AA0000"; // Color of the lines
     for (let i = 0; i < points - 1; i++) {
-        ctx.moveTo(pxlDlta * i, (1 - model.stats[i].loss / max) * height)
-        ctx.lineTo(pxlDlta * (i + 1), (1 - model.stats[i + 1].loss / max) * height)
-        ctx.stroke();
-        console.log("[" + i + "] " + pxlDlta * i + "," + (1 - model.stats[i].loss / max) * height)
+        grid.x1 = grid.left + pxlDlta * i
+        grid.x2 = grid.x1 + pxlDlta
+        grid.y1 = grid.top + (1 - model.stats[i].loss / max) * grid.height
+        grid.y2 = grid.top + (1 - model.stats[i + 1].loss / max) * grid.height
+
+        ctx.moveTo(grid.x1, grid.y1)
+        ctx.lineTo(grid.x2, grid.y2)
+        ctx.stroke()
     }
 
     return;
