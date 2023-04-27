d0a302bf383b224de318e876316e94c07556a160
icecoldgold773 <mikkel.tygesen@gmail.com>
2023-04-27 14:40:53 +0200

    Update

---
+++ node/public/js/main.js
@@ -0,0 +1,11 @@
+import { GetModel } from './fetch.js';
+import PlotGraph from './plot_graph.js';
+let model;
+
+try {
+    model = await GetModel('model1');
+}
+catch (err) {
+    console.error(err);
+}
+PlotGraph(model);
