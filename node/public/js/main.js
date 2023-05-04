0b4293dacb14dc17bec91dc49f702ac77a78c8b9
Oliver030500 <ovni22@student.aau.dk>
2023-05-04 13:29:26 +0200

    HTML

--- node/public/js/main.js
+++ node/public/js/main.js
@@ -1,13 +1,50 @@
 import { GetModel } from './fetch.js';
 import PlotGraph from './plot_graph.js';
+/*
+const buttons = document.querySelectorAll('.modelRightBtn');
 
-let model;
+const buttonPressed = e => {
+    console.log(e.target.id);  // Get ID of Clicked Element
+    console.log("hello")
+  }
 
-try {
-    model = await GetModel('model1');
+    buttons.addEventListener("click", buttonPressed);
+  */
+
+ /*let reply_click = function()
+{
+  console.log("Button clicked, id "+this.id);
 }
-catch (err) {
-    console.error(err);
-}
+document.getElementById('model1').onclick = reply_click;
+document.getElementById('model2').onclick = reply_click;
+document.getElementById('model3').onclick = reply_click;
 
-PlotGraph(model);
+
+document.querySelectorAll('.modelRightBtn').forEach(item => {
+    item.addEventListener('click', modelEvent)
+})
+
+const button = document.querySelector('#id');
+
+button.addEventListener('click', () => await GetModel('modelName'));
+*/
+
+/*
+async function modelEvent() {
+    await GetModel('modelName')
+    PlotGraph(model);
+}
+*/
+
+const buttons = document.querySelectorAll('.getModelButton');
+
+for (const button of buttons) {
+    button.addEventListener('click', e => { 
+        try {
+            GetModel(`${e.target.id}`).then(model => PlotGraph(model));
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+}
